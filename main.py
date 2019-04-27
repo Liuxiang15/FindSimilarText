04a4a5cb99b30c233ea5a3b601da56528630c506
Liuxiang15 <thss15_liux@163.com>
2019-04-27 19:39:43 +0800

    删除无用文件，获取文件下所有sheet的审查内容的函数

--- main.py
+++ main.py
@@ -146,24 +146,29 @@ def read_file(file_path):
         f.write(str(check_points))  # 把变量var写入test.txt。这里var必须是str格式，如果不是，则可以转一下。
     return check_points
 
-def read_excel_file(file_name, sheet_name):
+def read_excel_file(file_name, sheet_name_list):
+    #输入：文件名, 所有审查内容所在的表（list类型）
     #根据文件名来读取某个excel文件中的某一个sheet下的内容
-    xl = pd.ExcelFile('1-商业项目施工图审查要点-建筑.xlsx')
+    
+    xl = pd.ExcelFile(file_name)
     print(xl.sheet_names)
-    sheet = xl.parse('1.1建筑')
-    # print(sheet)
-    sheet_list = list(sheet.iloc[:, 2].values)
-    # print(sheet_list)
+    total_items = []
+    for sheet_name in sheet_name_list:
+        sheet = xl.parse(sheet_name)
+        total_items += list(sheet.iloc[:, 2].values)
+    print(total_items)
     exists_nan = True
     while exists_nan:
         #除掉所有nan
         exists_nan = False
-        for i in sheet_list:
+        for i in total_items:
                 if isinstance(i, float) and np.isnan(i):
                     exists_nan = True
-                    sheet_list.remove(i)
+                    total_items.remove(i)
+    print("total_items是--------------------------")
+    print(total_items)
+    return total_items
 
-    return sheet_list
 
 def read_txt(file_path):
     f = open(file_path,"r")   
@@ -214,7 +219,23 @@ def append_lines(file_path, lines):
     pass
     #输入:文件路径和
 
-test()
+# test()
+sheet_list = [
+    ['1.1建筑'],
+    [ '10.1幕墙'],
+    [ '11.1导向标识'],
+    [ '12.1夜景照明'],
+    [ '2.1试桩、试锚', '2.3土方开挖',  '2.5结构-桩基、抗浮锚杆',  '2.7结构-地下室',  '2.9结构-上部结构',  '2.11结构-幕墙、采光顶',  '2.13结构加固改造',  '2.15地质勘察', '2.16基坑支护及降水'],
+    [ '3.1 给排水'],
+    [ '4.1暖通'],
+    [ '5.1电气'],
+    [ '6.1弱电'],
+    [ '7.1内装'],
+    [ '审查要点'],
+    [ '9.1室外管线综合']
+]
+
+read_excel_file("总体设计审查要点/2-商业项目施工图审查要点-结构.xlsx", [ '2.1试桩、试锚', '2.3土方开挖',  '2.5结构-桩基、抗浮锚杆',  '2.7结构-地下室',  '2.9结构-上部结构',  '2.11结构-幕墙、采光顶',  '2.13结构加固改造',  '2.15地质勘察', '2.16基坑支护及降水'])
 
 
 
